a14a6d3e531bea9b161511e2546bccd830c7aa4c
rossiko22 <markospasovskq@gmail.com>
2026-04-08 03:37:30 +0200

    Clan 1

--- node_app/server.js
+++ node_app/server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     // /podatkovni-model/
     if (req.url === '/podatkovni-model/') {
 
-        fs.readFile('podatkovni-model.html', (err, data) => {
+        fs.readFile('podatkovni_model.html', (err, data) => {
             if (err) {
                 res.writeHead(500);
                 res.end('Napaka pri nalaganju HTML');
@@ -22,26 +22,26 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
 
         res.end(`
-REST API storitve za Fitness Buddy
+            REST API storitve za Fitness Buddy
 
-GET /users
-POST /users
+            GET /users
+            POST /users
 
-GET /workouts
-POST /workouts
-PUT /workouts/:id
-DELETE /workouts/:id
+            GET /workouts
+            POST /workouts
+            PUT /workouts/:id
+            DELETE /workouts/:id
 
-GET /habits
-POST /habits
+            GET /habits
+            POST /habits
 
-GET /habitlogs
-POST /habitlogs
+            GET /habitlogs
+            POST /habitlogs
 
-GET /meals
-POST /meals
-PUT /meals/:id
-DELETE /meals/:id
+            GET /meals
+            POST /meals
+            PUT /meals/:id
+            DELETE /meals/:id
         `);
     }
 
@@ -58,6 +58,47 @@ DELETE /meals/:id
         });
     }
 
+    // uml diagram slika
+    else if (req.url === '/uml_diagram.png') {
+        fs.readFile('uml_diagram.png', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end('Slika ni najdena');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'image/png' });
+                res.end(data);
+            }
+        });
+    }
+
+    // /funkcionalnosti_odjemalca/
+    else if (req.url === '/funkcionalnosti_odjemalca/') {
+
+        fs.readFile('funkcionalnosti_odjemalca.html', (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Napaka pri nalaganju HTML');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+
+    // /posebnosti/
+    else if (req.url === '/posebnosti/') {
+
+        fs.readFile('posebnosti.html', (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Napaka pri nalaganju HTML');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+
     else {
         res.writeHead(404);
         res.end('Stran ne obstaja');
@@ -65,4 +106,6 @@ DELETE /meals/:id
 
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log(`Strežnik deluje na vratih 3000...`);
+});
